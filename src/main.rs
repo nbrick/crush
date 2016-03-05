f61d7dae090679f84c365300eb445cbc699da27a
Nic Bricknell <nebricks@gmail.com>
2016-03-05 15:42:10 +0000

    Simplify Entry struct

--- src/main.rs
+++ src/main.rs
@@ -6,66 +6,36 @@ use std::collections::HashSet;
 
 
 #[derive(Debug)]
-enum Sign {
-    Plus,
-    Minus,
-}
-
-
-impl Sign {
-    pub fn from(literal: char) -> Result<Sign, String> {
-        match literal {
-            '+' => Ok(Sign::Plus),
-            '-' => Ok(Sign::Minus),
-            _   => Err(String::from("Called Sign::new() with neither '+' or '-'!")),
-        }
-    }
-}
-
-
-#[derive(Debug)]
-struct Amount {
+struct Delta {
     pence: i64,
-}
-
-
-impl Amount {
-    pub fn from(pounds_literal: &str) -> Result<Amount, String> {
-        // TODO: Account for decimal point.
-        Ok(Amount { pence: pounds_literal.parse().unwrap() })
-    }
 }
 
 
 #[derive(Debug)]
 struct Entry {
-    sign: Sign,
-    amount: Amount,  // pence
+    delta: Delta,
     tags: HashSet<String>,
 }
 
 
-
-// fn pounds_to_pence(pounds: String) -> Result<i64, String> {
-// }
-
-
 fn parseln(line: String) -> Result<Entry, String> {
-    
+
     let mut tokens = line.split_whitespace();
 
     //  Expect first token "+" or "-".
     let sign_slice = tokens.next().unwrap();
     assert!(sign_slice.len() == 1);
     let sign_literal = sign_slice.chars().nth(0).unwrap();
-    let sign = Sign::from(sign_literal).unwrap();
 
-    // Expect second token in the form "12.34".
-    let amount_literal = tokens.next().unwrap();
-    let amount = Amount::from(amount_literal).unwrap();
+    let amount = tokens.next().unwrap().parse().unwrap();
+    let delta = try!(match sign_literal {
+        '+' => Ok(Delta { pence: amount }),
+        '-' => Ok(Delta { pence: -amount }),
+        _ => Err(String::from("First token was neither '+' or '-'!")),
+    });
 
     // Remaining tokens are taken as tags.
-    Ok(Entry { sign: sign, amount: amount, tags: tokens.map(String::from).collect() })
+    Ok(Entry { delta: delta, tags: tokens.map(String::from).collect() })
 }
 
 
